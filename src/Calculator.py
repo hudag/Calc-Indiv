acb5de996f13641b24c61ec94854393b3de66bca
hudag <hudagohar16@gmail.com>
2019-11-11 20:47:50 -0500

    feature: Added the multiplication method

--- src/Calculator.py
+++ src/Calculator.py
@@ -5,6 +5,9 @@ def addition(a,b):
 def subtraction(a,b):
     return a-b
 
+def multiplication(a,b):
+    return a*b
+
 class Calculator:
     result=0
 
@@ -17,4 +20,8 @@ class Calculator:
 
     def subtract(self, a, b):
         self.result= subtraction(a,b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result= multiplication(a,b)
         return self.result
