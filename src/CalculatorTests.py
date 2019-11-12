12f50214a51a3ee97f809fac8ec126fb8811d365
hudag <hudagohar16@gmail.com>
2019-11-11 23:17:32 -0500

    Fix: subtraction test and reader

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -15,18 +15,17 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.result, 0)
 
     def test_add_method_calculator(self):
-        test_data = CsvReader('/src/Addition.csv').data
-        for row in test_data:
+        test_data1 = CsvReader('/src/Addition.csv').data
+        for row in test_data1:
             self.assertEqual(self.calculator.add(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_sub_method_calculator(self):
-        #test_data = CsvReader('/src/Subtraction.csv').data
-        #for row in test_data:
-         #   self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
-          #  self.assertEqual(self.calculator.result, int(row['Result']))
-        self.assertEqual(self.calculator.subtract(2,2),0)
-        self.assertEqual(self.calculator.result,0)
+        test_data2 = CsvReader('/src/Subtraction.csv').data
+        for row in test_data2:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
     def test_mult_method_calculator(self):
 
        self.assertEqual(self.calculator.multiply(3,3),9)
