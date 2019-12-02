561376242fc5bbe0bc1a01857a6ae09089cb62d0
hudag <hudagohar16@gmail.com>
2019-12-01 21:39:57 -0500

    feature: set up declar

--- SQLite/sqlalchemy_declarative.py
+++ SQLite/sqlalchemy_declarative.py
@@ -8,30 +8,33 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Customer(Base):
+    __tablename__ = 'Customer'
+
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name= Column(String(250))
+    last_name= Column(String(250))
+    username= Column(String(250))
+    email= Column(String(250))
+    address= Column(String(250))
+    town= Column(String(250))
 
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Item(Base):
+    __tablename__ = 'Item'
 
+    name= Column(Integer, primary_key=True)
+    cost_price= Column(String(250))
+    selling_price= Column(String(250))
+    quantity = Column(String(250))
 
-# Create an engine that stores data in the local directory's
-# sqlalchemy_example.db file.
+class Order(Base):
+    __tablename__ = 'Order'
+
+    order= Column(Integer, primary_key=True)
+    item= Column(String(250))
+    quantity= Column(String(250))
+
 engine = create_engine('sqlite:///sqlalchemy_example.db')
 
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
 Base.metadata.create_all(engine)
