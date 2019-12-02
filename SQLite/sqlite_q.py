3c0f3776b68a3fd689cf0f7d26d373775c5c88b9
hudag <hudagohar16@gmail.com>
2019-12-01 21:52:49 -0500

    feature: print query

--- SQLite/sqlite_q.py
+++ SQLite/sqlite_q.py
@@ -1,8 +1,68 @@
 import sqlite3
-
+from sqlalchemy import func
 conn = sqlite3.connect('example.db')
 
 c = conn.cursor()
+
+session.query(Customer).all()
+print(session.query(Customer))
+
+q = session.query(Customer)
+
+for c in q:
+    print(c.id, c.first_name)
+
+session.query(Customer.id, Customer.first_name).all()
+
+session.query(Customer).count()
+session.query(Item).count()
+session.query(Order).count()
+
+session.query(Customer).first()
+session.query(Item).first()
+session.query(Order).first()
+
+session.query(Customer).get(1)
+session.query(Item).get(1)
+session.query(Order).get(100)
+
+print(session.query(Customer).filter(Customer.first_name == 'John'))
+
+session.query(Customer).filter(Customer.id <= 5, Customer.town == "Norfolk").all()
+
+# find all customers who either live in Peterbrugh or Norfolk
+
+session.query(Customer).filter(or_(
+    Customer.town == 'Peterbrugh',
+    Customer.town == 'Norfolk'
+)).all()
+
+# find all customers whose first name is John and live in Norfolk
+
+session.query(Customer).filter(and_(
+    Customer.first_name == 'John',
+    Customer.town == 'Norfolk'
+)).all()
+
+# find all johns who don't live in Peterbrugh
+
+session.query(Customer).filter(and_(
+    Customer.first_name == 'John',
+    not_(
+        Customer.town == 'Peterbrugh',
+    )
+)).all()
+
+session.query(func.count(Customer.id)).join(Order).filter(
+    Customer.first_name == 'John',
+    Customer.last_name == 'Green',
+).group_by(Customer.id).scalar()
+
+session.query(
+    func.count("*").label('town_count'),
+    Customer.town
+).group_by(Customer.town).having(func.count("*") > 2).all()
+
 c.execute('SELECT * FROM person')
 print
 c.fetchall()
