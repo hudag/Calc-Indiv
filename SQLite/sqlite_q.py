33bee6b16f57c2f9dec5e5953c7a2cb70041da5b
hudag <hudagohar16@gmail.com>
2019-12-01 22:05:24 -0500

    Feature: unions

--- SQLite/sqlite_q.py
+++ SQLite/sqlite_q.py
@@ -71,6 +71,10 @@ session.query(
     cast("2010-12-01", Date),
 ).all()
 
+s1 = session.query(Item.id, Item.name).filter(Item.name.like("Wa%"))
+s2 = session.query(Item.id, Item.name).filter(Item.name.like("%e%"))
+s1.union(s2).all()
+s1.union_all(s2).all()
 
 c.execute('SELECT * FROM person')
 print
