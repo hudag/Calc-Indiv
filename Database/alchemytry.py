d8dc8bfa4d6925ec53c62166fb7232003f2cfb68
hudag <hudagohar16@gmail.com>
2019-12-08 02:37:22 -0500

    task: add query

--- Database/alchemytry.py
+++ Database/alchemytry.py
@@ -139,3 +139,86 @@ o3.order_lines.append(orderline2)
 session.add_all([o3])
 
 session.commit()
+
+conn = sqlite3.connect('example.db')
+
+c = conn.cursor()
+
+session.query(Customer).all()
+print(session.query(Customer))
+
+q = session.query(Customer)
+
+for c in q:
+    print(c.id, c.first_name)
+
+session.query(Customer.id, Customer.first_name).all()
+
+session.query(Customer).count()
+session.query(Item).count()
+session.query(Order).count()
+
+session.query(Customer).first()
+session.query(Item).first()
+session.query(Order).first()
+
+session.query(Customer).get(1)
+session.query(Item).get(1)
+session.query(Order).get(100)
+
+print(session.query(Customer).filter(Customer.first_name == 'John'))
+
+session.query(Customer).filter(Customer.id <= 5, Customer.town == "Norfolk").all()
+
+# find all customers who either live in Peterbrugh or Norfolk
+
+session.query(Customer).filter(or_(
+    Customer.town == 'Peterbrugh',
+    Customer.town == 'Norfolk'
+)).all()
+
+# find all customers whose first name is John and live in Norfolk
+
+session.query(Customer).filter(and_(
+    Customer.first_name == 'John',
+    Customer.town == 'Norfolk'
+)).all()
+
+# find all johns who don't live in Peterbrugh
+
+session.query(Customer).filter(and_(
+    Customer.first_name == 'John',
+    not_(
+        Customer.town == 'Peterbrugh',
+    )
+)).all()
+
+session.query(func.count(Customer.id)).join(Order).filter(
+    Customer.first_name == 'John',
+    Customer.last_name == 'Green',
+).group_by(Customer.id).scalar()
+
+session.query(
+    func.count("*").label('town_count'),
+    Customer.town
+).group_by(Customer.town).having(func.count("*") > 2).all()
+
+session.query(
+    cast(func.pi(), Integer),
+    cast(func.pi(), Numeric(10,2)),
+    cast("2010-12-01", DateTime),
+    cast("2010-12-01", Date),
+).all()
+
+s1 = session.query(Item.id, Item.name).filter(Item.name.like("Wa%"))
+s2 = session.query(Item.id, Item.name).filter(Item.name.like("%e%"))
+s1.union(s2).all()
+s1.union_all(s2).all()
+
+c.execute('SELECT * FROM person')
+print
+c.fetchall()
+c.execute('SELECT * FROM address')
+print
+c.fetchall()
+conn.close()
